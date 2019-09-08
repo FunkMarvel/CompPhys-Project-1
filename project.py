21290058e9537aac24576a4b9587547de13d81ab
Anders Petershagen Åsbø <aanders35@gmail.com>
2019-09-08 16:56:10 +0200

    Fixed bugs in decomp_and_forward_sub, and backward_sub. Integration loops now work correctly.

--- project.py
+++ project.py
@@ -1,7 +1,5 @@
 # Project 1 FYS3150, Anders P. Åsbø
-from numba import jit
-from data_generator import generate_data
-import sys
+import data_generator as gen
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,36 +7,44 @@ N = int(input("Specify number of data points N: "))
 name = input("Name of data-set without file extension: ")
 
 x = np.linspace(0, 1, N)  # array of normalized positions.
-generate_data(x, name)  # generating data set.
-h = (x[-1]-x[0])/N  # defining step-siz.
+h = (x[0]-x[-1])/N  # defining step-siz.
 
-u = np.empty(N-2)  # array for unkown values.
-d = np.empty(N-2)  # array for diagonal elements.
-d_prime = np.empty(N-2)  # array for diagonal after decom. and sub.
-a = np.empty(N-3)  # array for upper, off-center diagonal.
-b = np.empty(N-3)  # array for lower, off-center diagonal.
-g = np.fromfile("%s.dat" % name)  # array for g in matrix eq. Au=g.
+gen.generate_data(x, name)  # generating data set.
+anal_sol = np.loadtxt("analytical_solution_for_%s.dat" % name)
+
+u = np.empty(N)  # array for unkown values.
+d = np.full(N, 2)  # array for diagonal elements.
+d_prime = np.empty(N)  # array for diagonal after decom. and sub.
+a = np.full(N-1, -1)  # array for upper, off-center diagonal.
+b = np.full(N-1, -1)  # array for lower, off-center diagonal.
+g = np.loadtxt("%s.dat" % name)*h**2  # array for g in matrix eq. Au=g.
 g_prime = np.empty(N)  # array for g after decomp. and sub.
 
 
 def main():
-    pass
+    decomp_and_forward_sub()
+    backward_sub()
 
 
-@jit(nopython=True)
 def decomp_and_forward_sub():
     """Function that performs the matrix decomposition and forward
     substitution."""
-    for i in range(len(u)):
+    d_prime[0] = d[0]
+    g_prime[0] = g[0]
+    for i in range(1, len(u)):
         d_prime[i] = d[i] - b[i-1]*a[i-1]/d_prime[i-1]
-        g_prime[i] = g[i] - b[i-1]*g[i-1]/d_prime[i-1]
+        g_prime[i] = g[i] - b[i-1]*g_prime[i-1]/d_prime[i-1]
 
 
-@jit(nopython=True)
 def backward_sub():
     """Function that performs the backward substitution."""
-    for i in range(len(u)):
-        u[-i] = (g_prime[-i]-a[-i]*u[-(i+1)])/d_prime[-i]
+    u[0], u[-1] = 0, 0
+    for i in reversed(range(1, len(u)-1)):
+        u[i] = (g_prime[i]-a[i]*u[i+1])/d_prime[i]
+
+
+def plot_solutions():
+    pass
 
 
 if __name__ == '__main__':
