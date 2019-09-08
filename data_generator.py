26777b15e7d1d7d45287ce5c82c4a6ff128465a6
Anders Petershagen Åsbø <aanders35@gmail.com>
2019-09-08 16:54:39 +0200

    Added method for generating random tridiagonal matrix.

--- data_generator.py
+++ data_generator.py
@@ -3,10 +3,8 @@ import numpy as np
 
 
 def main():
-    """Generates test data if run as stand-alone program."""
-    x = np.linspace(0, 1, 1001)
-    test_name = "Test_data"
-    generate_data(x, test_name)
+    test_generate_data()
+    test_generate_tridiagonal()
 
 
 def generate_data(x, name):
@@ -18,6 +16,29 @@ def generate_data(x, name):
     analytical_solution = 1-(1-np.exp(-10))*x-np.exp(-10*x)
     anal_name = "analytical_solution_for_%s.dat" % name
     np.savetxt(anal_name, analytical_solution)
+
+
+def generate_tridiagonal(N):
+    """Function that generates a Nx3 array with each column corresponding to
+    the non-zero elements in a tridiagonal matrix.
+    "b" (mat_data[:,0]) is the lower diagonal,
+    "d" (mat_data[:,1]) is the diagonal,
+    and "a" (mat_data[:,2]) is the upper diagonal."""
+    mat_data = np.random.randint(1, 100, size=(N, 3))
+    np.savetxt("b-d-a_tridiagonal.dat", mat_data, fmt="%f")
+
+
+def test_generate_data():
+    """Generates test data if run as stand-alone program."""
+    x = np.linspace(0, 1, 1001)
+    test_name = "Test_data"
+    generate_data(x, test_name)
+
+
+def test_generate_tridiagonal():
+    """Generates test data for tridiagonal."""
+    generate_tridiagonal(100)
+    print(np.loadtxt("b-d-a_tridiagonal.dat"))
 
 
 if __name__ == '__main__':
